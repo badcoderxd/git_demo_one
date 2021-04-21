9144b6210aa41e2b4dcb59f0e8bc1e0b114ef3c8
Akshay <53929892+badcoderxd@users.noreply.github.com>
2021-04-21 20:50:59 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -13,6 +13,9 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     }
+   email:{
+    type:string,
+    required:true
 })
 
 mongoose.model("User",userSchema)
